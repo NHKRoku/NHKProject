0ceb4d43acb8d0063d5e5fe7c9d196f8666a423e
NHKRoku <134927860+NHKRoku@users.noreply.github.com>
2023-05-30 17:08:08 +0900

    Update nhkad.sh

--- nhkad.sh
+++ nhkad.sh
@@ -1,45 +1,26 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 if ! [ -x "$(command -v screen)"  ] ; then
-
 apt install screen -y
-
 fi
 
 if ! [ -x "$(command -v AdGuardHome)"  ] ; then
-
 cd ~
-
-wget https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.29/AdGuardHome_linux_arm64.tar.gz
-
+wget https://static.adguard.com/adguardhome/release/AdGuardHome_linux_arm64.tar.gz
 tar -zxvf AdGuardHome_linux_arm64.tar.gz
-
 rm -rf AdGuardHome_linux_arm64.tar.gz
-
 chmod +x AdGuardHome
-
 cd AdGuardHome
-
 cp AdGuardHome /data/data/com.termux/files/usr/bin/
-
 cd ~
-
 screen -dmS Ad sudo AdGuardHome
-
-echo -e "\033[31m Chrome ブラウザを使用して開いてください、127.0.0.1:3000\033[0m"
-
+echo -e "\033[31mChrome ブラウザで開いてください、127.0.0.1:3000\033[0m"
 echo ""
-
 am start -a android.intent.action.VIEW -d http://127.0.0.1:3000
 
 else
-
 screen -dmS Ad sudo AdGuardHome
-
-echo -e "\033[31m Chrome ブラウザを使用して開いてください、127.0.0.1\033[0m"
-
+echo -e "\033[31mChrome ブラウザで開いてください、127.0.0.1\033[0m"
 echo ""
-
 am start -a android.intent.action.VIEW -d http://127.0.0.1
-
 fi
